6c808a716e0400a79ffa015891ef736670e355b2
berkevlk <74499923+berkevlk@users.noreply.github.com>
2021-04-03 11:21:19 +0300

    Update hesapmakinesi.py

--- python/projeler/hesapmakinesi.py
+++ python/projeler/hesapmakinesi.py
@@ -1,9 +1,9 @@
-num1=int(input('1. sayıyı giriniz  '))
-num2=int(input('2. sayıyı giriniz  '))
+num1=int(input('1. sayıyı giriniz  ')) #buradaki int komutu tam sayı girilmesini sağlar
+num2=int(input('2. sayıyı giriniz  '))#buradaki input değeri kullnıcıdan değer alır
 op=input('işlemi seçiniz(+,-,*,/)  ')
-if op=='*':
+if op=='*': #buradaki yazılan koşullar uygunsa altındaki kodu çalıştırır.
     print(num1, '*', num2,'=', num1*num2)
-elif op=='+':
+elif op=='+': #elif komutu değilse eğer anlamı katar.yani else if yazmak yerine komut yazmayı hızlandırır.
  print(num1, '+', num2,'=', num1+num2)
 elif op == '-':
  print(num1, '-', num2, '=', num1-num2)

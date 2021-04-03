71e1d5bd6d686bad02f351013b591c9b4d33172c
berkevlk <74499923+berkevlk@users.noreply.github.com>
2021-04-03 10:46:59 +0300

    Create hesapmakinesi.py

---
+++ python/projeler/hesapmakinesi.py
@@ -0,0 +1,11 @@
+num1=int(input('1. sayıyı giriniz  '))
+num2=int(input('2. sayıyı giriniz  '))
+op=input('işlemi seçiniz(+,-,*,/)  ')
+if op=='*':
+    print(num1, '*', num2,'=', num1*num2)
+elif op=='+':
+ print(num1, '+', num2,'=', num1+num2)
+elif op == '-':
+ print(num1, '-', num2, '=', num1-num2)
+elif op=='/':
+ print(num1,'/',num2,'=', num1/num2)

2cca5a844951cb73507026d3b18dcfa81662fec7
berkevlk <74499923+berkevlk@users.noreply.github.com>
2021-04-05 13:43:51 +0300

    Add files via upload

---
+++ python/projeler/ortalama hesaplama.py
@@ -0,0 +1,7 @@
+not1=int(input('1. notu giriniz'))
+not2=int(input('2. notu giriniz'))
+not3=int(input('Performans notu giriniz'))
+toplam=(not1+not2+not3)
+
+print('Lütfen bekleyiniz')
+print(toplam/3)
